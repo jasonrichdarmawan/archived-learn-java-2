756f68c658cc8713d8249e3a67b7fe1fd52dc856
kidfrom <jason.onggo@tempatkerja.com>
2020-08-07 19:09:24 +0700

    add switch case example

--- src/learn_ifelse/Main.java
+++ src/learn_ifelse/Main.java
@@ -4,7 +4,7 @@ public class Main {
     public static void main (String[] args) {
         System.out.println("start");
 
-        int a = 1;
+        int a = 10;
 
         if (a == 1) {
             System.out.println("integer a value: " + a);
@@ -29,5 +29,24 @@ public class Main {
         }
 
         System.out.println("End: Nested If Else Statement");
+
+        // switch case
+
+        System.out.println("Start: Switch Case");
+
+        switch (a) {
+            case 10:
+            System.out.println("integer a value: " + a);
+            case 1:
+            System.out.println("case 1: should not System.out.print. Use break after each statement" + a);
+            System.out.println("integer a value: " + a); // should not out.
+            break;
+            case 5:
+            System.out.println("case 5: should not System.out.print." + a);
+            default:
+            System.out.println("default case, similar to else statement" + a);
+        }
+
+        System.out.println("End: Switch Case");
     }
 }
