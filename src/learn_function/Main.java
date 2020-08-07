852ca081d51167fba179e888c0cb71e71e5cba46
kidfrom <jason.onggo@tempatkerja.com>
2020-08-07 22:49:13 +0700

    add method example

---
+++ src/learn_function/Main.java
@@ -0,0 +1,17 @@
+package learn_function;
+
+public class Main {
+    public static void main (String[] args) {
+        int y;
+        y = calculate(5);
+        System.out.println("(5 + 2) * 10: " + y);
+
+    }
+
+    // method
+    private static int calculate (int x) {
+        int result;
+        result = (x + 2) * 10;
+        return result;
+    }
+}
