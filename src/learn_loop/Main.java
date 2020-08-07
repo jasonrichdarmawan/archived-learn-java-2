82d459a4471bb05afcca86edb03aefc3c39a571d
kidfrom <jason.onggo@tempatkerja.com>
2020-08-07 21:22:36 +0700

    add for loop and nested for loop example

--- src/learn_loop/Main.java
+++ src/learn_loop/Main.java
@@ -5,6 +5,7 @@ public class Main {
         // loop: while, do while, for, recursive
 
         // while loop
+        // Start -> condition -> if true statement
         System.out.println("Start: while loop");
 
         int a = 0;
@@ -18,6 +19,7 @@ public class Main {
 
         // do while loop
         // the do is outside the while body;
+        // Start -> do -> condition -> if true loop
         System.out.println("Start: do while loop");
 
         a = 0;
@@ -28,5 +30,27 @@ public class Main {
         } while (a < 0);
 
         System.out.println("End: do while loop");
+
+        // for loop
+        // the statement is inside the for loop body.
+        // Start -> condition -> if true statement
+        System.out.println("Start: for loop");
+
+        for (int b = 0; b < 0; b++) {
+            System.out.println("value of variable b: " + b);
+        }
+
+        System.out.println("End: for loop");
+
+        // nested for loop
+        System.out.println("Start: nested for loop");
+
+        for (int b = 0; b < 3; b++) {
+            for (int c = 0; c < 3; c++) {
+                System.out.println("variable b: " + b + " variable c: " + c);
+            }
+        }
+
+        System.out.println("End: nested for loop");
     }
 }
