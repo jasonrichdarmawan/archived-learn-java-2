7d3b9ec2fc196cfa46666b950a243be6559436d6
kidfrom <jason.onggo@tempatkerja.com>
2020-08-09 07:17:12 +0700

    add example with try, catch, finally

---
+++ src/learn_error/Main.java
@@ -0,0 +1,53 @@
+package learn_error;
+
+import java.io.FileInputStream;
+import java.io.IOException;
+import java.util.Scanner;
+
+public class Main {
+    public static void main (String[] args) {
+        int[] array = {0,1,2,3};
+
+        Scanner userInput = new Scanner(System.in);
+
+        int index = userInput.nextInt();
+        userInput.close();
+
+        // exception handling: try, catch, finally
+        // try {
+        //     System.out.println("value of index no. " + index + " is " + array[index]);
+        // } catch (Exception e) {
+        //     System.err.println(e);
+        // }
+
+        FileInputStream fileInput = null;
+
+        // runtime error
+        // try { 
+        //     fileInput = new FileInputStream("test.txt");
+        // } catch (IOException e) {
+        //     System.err.println(e);
+        // }
+
+        // try {
+        //     System.out.println("value of index no. " + index + " is " + array[index]);
+        //     fileInput = new FileInputStream("test.txt");
+        // } catch (ArrayIndexOutOfBoundsException e) {
+        //     System.err.println(e);
+        // } catch (IOException e) {
+        //     System.err.println(e);
+        // }
+
+        // finally
+
+        try {
+            System.out.println("value of index no. " + index + " is " + array[index]);
+        } catch (ArrayIndexOutOfBoundsException e) {
+            System.err.println(e);
+        } finally {
+            System.out.println("finally");
+        }
+
+        System.out.println("End of program");
+    }
+}
